38da65ee56c075e420a09ff722ef8029630da9c3
RedPanda <lizzy.maddog@gmail.com>
2016-01-07 19:51:00 -0200

    Fix code with Rubocop

--- spec/features/user_list_clients_spec.rb
+++ spec/features/user_list_clients_spec.rb
@@ -1,21 +1,12 @@
 require 'rails_helper'
 
-  feature 'User list all clients' do
-    scenario  'successfully' do
+feature 'User list all clients' do
+  scenario 'successfully' do
+    create(:client)
 
-      # client = Client.create( razao_social: "Campus Code",
-      #                         cnpj: "26.286.134/0001-04",
-      #                         nome_contato: "Gustavo",
-      #                         telefone: "555-555",
-      #                         email: "g@code.com")
+    visit clients_path
 
-      client = create(:client)
-
-      visit clients_path
-
-      expect(page).to have_content('Campus Code')
-      expect(page).to have_content('26.286.134/0001-04')
-    end
-
-
+    expect(page).to have_content('Campus Code')
+    expect(page).to have_content('26.286.134/0001-04')
   end
+end
