65892c1108bfbe818c10a510b8f365994be80229
Cezar Vaz <cezarvaz@gmail.com>
2016-01-18 22:31:15 -0200

    Add unit test

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -7,11 +7,27 @@ describe Contract do
       expect(contract.print_tools).to eq ''
     end
 
+    it 'one tool' do
+      contract = build(:contract)
+      contract.tools << build(:tool, name: 'Furadeira')
+      expect(contract.print_tools).to eq 'Furadeira'
+    end
+
     it 'two tools' do
       contract = build(:contract)
       contract.tools << build(:tool, name: 'Furadeira')
       contract.tools << build(:tool, name: 'Britadeira')
       expect(contract.print_tools).to eq 'Furadeira, Britadeira'
+    end
+
+    it 'five tools' do
+      contract = build(:contract)
+      contract.tools << build(:tool, name: 'Furadeira')
+      contract.tools << build(:tool, name: 'Britadeira')
+      contract.tools << build(:tool, name: 'Betoneira')
+      contract.tools << build(:tool, name: 'Marteladeira')
+      contract.tools << build(:tool, name: 'Andaime')
+      expect(contract.print_tools).to eq 'Furadeira, Britadeira, Betoneira, Marteladeira, Andaime'
     end
   end
 end
